17adf83df983be67420a1c0f2f520dbdb0f42b20
priya6691 <easwarapriyas@gmail.com>
2021-11-29 23:13:18 -0500

    First commit
    This is the first commit that can be shared for the solution obtained for the coding challenge

---
+++ solution.py
@@ -0,0 +1,84 @@
+
+import pandas as pd
+import validators
+import boto3
+import io
+import os
+from string import ascii_lowercase
+from urllib.parse import urlparse
+
+def merge_files_from_url(path):
+
+    master = pd.DataFrame()
+
+    ## Iterate through ascii lowercase and generate file paths
+    for c in ascii_lowercase:
+        file = path + c + ".csv"
+        master = master.append(pd.read_csv(file))
+
+    if master.empty:
+        ## When no files available to process create an empty file and return
+        master.to_csv('MergedFromUrl.csv')
+        print('Empty file created in the path', os.getcwd(), 'File name : MergedFromUrl.csv')
+        return ()
+
+    transposed = master.pivot(index='user_id', columns='path', values='length').reset_index()
+
+    #Replacing NA with 0
+    transposed = transposed.fillna(0)
+    #print(transposed.to_string(index=False))
+    transposed.to_csv('MergedFromUrl.csv',index=False)
+    print('Files are processed and merged file is successfully created in the path :',os.getcwd(), 'File name : MergedFromUrl.csv')
+
+def merge_files_from_s3(bucket):
+
+    # Please provide a valid access key id and key for authentication
+    s3_client = boto3.client('s3', aws_access_key_id='<aws_access_key_id>',
+                             aws_secret_access_key='<aws_secret_access_key>')
+
+    result = s3_client.list_objects_v2(Bucket=bucket, Delimiter='/*')
+    master = pd.DataFrame()
+
+    for r in result["Contents"]:
+        response = s3_client.get_object(Bucket=bucket, Key=r["Key"])
+        master = master.append(pd.read_csv(io.BytesIO(response['Body'].read())))
+
+    if master.empty:
+        ## When no files available to process create an empty file and return
+        master.to_csv('MergedFromS3.csv')
+        print('Files are processed and merged file is successfully created in the path :', os.getcwd(), 'File name : MergedFromS3.csv')
+        return ()
+
+    transposed = master.pivot(index='user_id', columns='path', values='length').reset_index()
+    transposed = transposed.fillna(0)
+    #print(transposed.to_string(index=False))
+    transposed.to_csv('MergedFromS3.csv', index=False)
+    print('File is successfully created in the path :', os.getcwd(), 'File name : MergedFromS3.csv')
+
+
+if __name__ == '__main__':
+    # path = r'https://public.wiwdata.com/engineering-challenge/data/'
+    path = input("Enter the public root url or an S3 url:")
+
+    # Check if path is provided
+    if path == 'undefined' or path == '':
+        raise Exception('Input path is empty. Please try again with a valid path')
+
+    if 's3' not in path and validators.url(path) == True:
+        merge_files_from_url(path)
+    elif 's3' in path and validators.url(path) == True:
+        o = urlparse(path, allow_fragments=False)
+        bucket = o.netloc.split('.')
+        print('Thanks for providing an S3 url. Extracted Bucket name is : ',bucket[0], 'Processing files ..')
+        merge_files_from_s3(bucket[0])
+    elif 's3' in path:
+        o = urlparse(path, allow_fragments=False)
+        bucket = o.netloc.split('.')
+        print('Thanks for providing an S3 url. Extracted Bucket name is : ', bucket[0], 'Processing files ..')
+        merge_files_from_s3(bucket[0])
+    else:
+        print('Please enter a valid url or S3 Bucket name')
+
+
+
+
